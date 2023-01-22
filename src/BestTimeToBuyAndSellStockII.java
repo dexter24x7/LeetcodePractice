a9aa4b7618db8ac92be88b646edf7e7ed1bd9d2f
dexter24x7 <117462852+dexter24x7@users.noreply.github.com>
2023-01-22 16:52:38 +0530

    Problem 122

---
+++ src/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,21 @@
+//122. Best Time to Buy and Sell Stock II
+/*
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time.
+However, you can buy it then immediately sell it on the same day.
+Find and return the maximum profit you can achieve.
+ */
+public class BestTimeToBuyAndSellStockII {
+    static int maxProfit(int[] prices) {
+        int profit=0, size=prices.length;
+        for (int i = 1; i < size; i++) {
+            if (prices[i]>prices[i-1])
+                profit += (prices[i] - prices[i-1]);
+        }
+        return profit;
+    }
+    public static void main(String[] args) {
+        int[] prices = {1,2,3,4,5};
+        System.out.println(maxProfit(prices));
+    }
+}
