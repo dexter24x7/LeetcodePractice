93a63f4912ca946b22b5eb0e973ebc28bc2f95c7
dexter24x7 <117462852+dexter24x7@users.noreply.github.com>
2023-01-24 21:22:12 +0530

    Problem 62

--- src/UniquePaths.java
+++ src/UniquePaths.java
@@ -1,8 +1,16 @@
+//62. Unique Paths
+/*
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]).
+The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
+The robot can only move either down or right at any point in time.
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+ */
 public class UniquePaths {
     public static void main(String[] args) {
         int m=6, n=6;
-        System.out.println("Brute Force Approach");
-        System.out.println(uniquePaths(m-1,n-1));
+//        System.out.println("Brute Force Approach");
+//        System.out.println(uniquePaths(m-1,n-1));
         System.out.println("Optimised Approach");
         System.out.println(uniquePaths2(m-1,n-1));
     }
