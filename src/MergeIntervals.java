40acd4c109e252d0843ca75d0f8fbba08f97ba5f
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-02-02 23:30:14 +0530

    Problem 56

---
+++ src/MergeIntervals.java
@@ -0,0 +1,14 @@
+//56. Merge Intervals
+/*
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ */
+
+public class MergeIntervals {
+    public static void main(String[] args) {
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+    }
+    public int[][] merge(int[][] intervals) {
+
+    }
+}
