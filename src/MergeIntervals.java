5f6b560ad2b6af8b96af4c2021769afa7683db9d
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-02-03 01:40:16 +0530

    Problem 56

--- src/MergeIntervals.java
+++ src/MergeIntervals.java
@@ -4,11 +4,35 @@ Given an array of intervals where intervals[i] = [starti, endi], merge all overl
 and return an array of the non-overlapping intervals that cover all the intervals in the input.
  */
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MergeIntervals {
     public static void main(String[] args) {
-        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
-    }
-    public int[][] merge(int[][] intervals) {
+        int[][] intervals = {{5,7},{1,3},{4,5}};
 
+        System.out.println(Arrays.deepToString(merge(intervals)));
+    }
+    public static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(ints -> ints[0]));
+        int[][] res = new int[intervals.length][2];
+        res[0] = intervals[0];
+        int pos=0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (res[pos][1]>=intervals[i][0]) {
+                res[pos][0]= Math.min(res[pos][0], intervals[i][0]);
+                res[pos][1]= Math.max(res[pos][1], intervals[i][1]);
+            }
+            else {
+                pos++;
+                res[pos][0]= intervals[i][0];
+                res[pos][1]= intervals[i][1];
+            }
+        }
+        int[][] trimArray = new int[pos+1][2];
+        for (int i = 0; i <= pos; i++) {
+            System.arraycopy(res[i], 0, trimArray[i], 0, 2);
+        }
+        return trimArray;
     }
 }
