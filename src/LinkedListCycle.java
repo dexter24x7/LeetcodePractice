66f8f6b471b2c4dec374f979368598fe64060aac
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-02-05 01:18:25 +0530

    Problem 141

--- src/LinkedListCycle.java
+++ src/LinkedListCycle.java
@@ -1,5 +1,3 @@
-import java.util.LinkedList;
-
 //141. Linked List Cycle
 /*
 Given head, the head of a linked list, determine if the linked list has a cycle in it.
@@ -9,6 +7,8 @@ node that tail's next pointer is connected to. Note that pos is not passed as a 
 Return true if there is a cycle in the linked list. Otherwise, return false.
  */
 public class LinkedListCycle {
+
+    //user defined linked-list class
     static class ListNode {
       int val;
       ListNode next;
@@ -18,30 +18,27 @@ public class LinkedListCycle {
       }
   }
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1,2,3,4,5,6};
         ListNode head = new ListNode(-1);
         ListNode curr = head;
         for (int j : arr) {
             curr.next = new ListNode(j);
             curr = curr.next;
         }
-        System.out.println(hasCycle(head));
+        curr.next = head.next; //linked the last node to first node to make linked list cyclic
+        System.out.println(hasCycle(head));//passing the head to check if cycle exists in the Linked list
 
     }
     public static boolean hasCycle(ListNode head) {
-        int size=0;
-        ListNode curr = head;
-        while (curr.next!=null){
-            curr = curr.next;
-            size++;
-        }
-        ListNode pos = curr;
-        curr = head;
-        while (curr.next!=null){
-            if (curr==pos.next)
+        if(head == null || head.next==null) return false; //for 0 node LL and 1 node non-cyclic LL
+        ListNode fastPtr = head.next.next, slowPtr= head.next; //slowPointer at first element and fastPointer at second element
+        while (fastPtr!=null && fastPtr.next!=null){ //check if fastPointer has reached the end of linked-list
+            //if the fastPointer is equal or behind the slowPointer it must have cycled once
+            if (fastPtr==slowPtr || fastPtr.next==slowPtr)
                 return true;
-            curr = curr.next;
+            slowPtr = slowPtr.next; //1x speed
+            fastPtr = fastPtr.next.next; //2x speed
         }
-        return false;
+        return false; //fastPointer has reached the end of linked-list
     }
 }
