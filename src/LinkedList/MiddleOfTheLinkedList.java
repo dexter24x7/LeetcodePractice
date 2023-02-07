adbe8f1d231de96fc23a61a32b83741769a09473
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-02-07 23:37:55 +0530

    Problem 876

---
+++ src/LinkedList/MiddleOfTheLinkedList.java
@@ -0,0 +1,38 @@
+package LinkedList;
+//876. Middle of the Linked List
+/*
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+ */
+public class MiddleOfTheLinkedList {
+    static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(){}
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        ListNode head = new ListNode(-1);
+        ListNode curr = head;
+        for (int j : arr) {
+            curr.next = new ListNode(j);
+            curr = curr.next;
+        }
+        ListNode mid = middleNode(head);
+        System.out.println(mid.val);
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode fast= new ListNode(), slow = new ListNode();
+        fast.next = head; slow.next = head;
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow.next;
+    }
+}
