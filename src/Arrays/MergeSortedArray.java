f277eb48199be026e64907f908bcd4e8e3dfbb57
dexter24x7 <117462852+shubhjena@users.noreply.github.com>
2023-03-05 08:06:04 +0530

    update the MergeSortedArray

--- src/Arrays/MergeSortedArray.java
+++ src/Arrays/MergeSortedArray.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class MergeSortedArray {
     public static void main(String[] args) {
     int[] nums1= {1,2,3,0,0,0};
@@ -9,14 +11,14 @@ public class MergeSortedArray {
     }
 
     static void merge(int[] nums1, int m, int[] nums2, int n) {
+        //compare the arrays in reverse order and fill nums1 from end
         while(m>0 && n>0){
             nums1[m+n-1]=nums1[m-1]>nums2[n-1]?nums1[--m] :nums2[--n];
         }
+        //if elements in nums1 get exhausted
         while (n>0)
             nums1[--n]=nums2[n];
-        System.out.print("[");
-        for (int num : nums1)
-            System.out.print(num+", ");
-        System.out.println("]");
+        //print he merged array
+        System.out.println(Arrays.toString(nums1));
     }
 }
