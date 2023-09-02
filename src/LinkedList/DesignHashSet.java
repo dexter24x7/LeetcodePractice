19a47555bc1bf19f2d91c40a9854e35800141069
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-09-03 04:02:25 +0530

    add DesignHashSet to LinkedList package.

---
+++ src/LinkedList/DesignHashSet.java
@@ -0,0 +1,27 @@
+package LinkedList;
+//705. Design HashSet
+/*
+Design a HashSet without using any built-in hash table libraries.
+Implement MyHashSet class:
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+ */
+public class DesignHashSet {
+    boolean[] set;
+    public DesignHashSet() {
+        set = new boolean[1000001];
+    }
+
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
