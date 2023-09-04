43f173c448e9a0afc1dff1093a738928ef9a353a
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-09-04 21:00:25 +0530

    add SearchInRotatedSortedArray to BinarySearch package

--- src/BinarySearch/SearchInRotatedSortedArray.java
+++ src/BinarySearch/SearchInRotatedSortedArray.java
@@ -8,6 +8,7 @@ For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6
 Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums,
 or -1 if it is not in nums. You must write an algorithm with O(log n) runtime complexity.
  */
+
 public class SearchInRotatedSortedArray {
     public static int search(int[] nums, int target) {
         int n = nums.length;
