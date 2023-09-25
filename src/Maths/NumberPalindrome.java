bdc655cefb3ae5666d2807ae0921f4f037ed7480
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-09-25 23:59:41 +0530

    refactor NumberPalindrome in Maths package

--- src/Maths/NumberPalindrome.java
+++ src/Maths/NumberPalindrome.java
@@ -1,5 +1,8 @@
 package Maths;
-
+//9. Palindrome Number
+/*
+Given an integer x, return true if x is a palindrome, and false otherwise.
+ */
 import java.util.Scanner;
 public class NumberPalindrome {
     public static boolean isNumberPalindrome(int x){
@@ -12,10 +15,24 @@ public class NumberPalindrome {
         }
         return number == x;
     }
+    //Approach 2
+    public static boolean isPalindrome(int x) {
+        if(x<0) return false;
+        String num = Integer.toString(x);
+        int l=0;
+        int r=num.length()-1;
+        while(l<r){
+            if(num.charAt(l)!=num.charAt(r)) return false;
+            l++; r--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the number:");
     int x= sc.nextInt();
         System.out.println(isNumberPalindrome(x));
+        System.out.println(isPalindrome(x));
     }
 }
