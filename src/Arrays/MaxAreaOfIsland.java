541a73adb0e4dd4d69084167b2835558ade08180
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-10-18 22:47:17 +0530

    refactor MaxAreaOfIsland in Arrays Package

--- src/Arrays/MaxAreaOfIsland.java
+++ src/Arrays/MaxAreaOfIsland.java
@@ -29,13 +29,15 @@ public class MaxAreaOfIsland {
     }
     //function to get island size
     public static int dfs(int[][] grid,int i, int j){
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length)
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length ||
+                grid[i][j]==0){
             return 0;
-        else if(grid[i][j] ==1){
-            grid[i][j]=0;
-            return 1+dfs(grid,i+1,j)+dfs(grid,i-1,j)+dfs(grid,i,j+1)+dfs(grid,i,j)+dfs(grid,i,j-1);
         }
-        return 0;
+        grid[i][j]=0;
+        return 1+dfs(grid,i+1,j)+
+                dfs(grid,i-1,j)+
+                dfs(grid,i,j+1)+
+                dfs(grid,i,j-1);
     }
 
     public static void main(String[] args) {
