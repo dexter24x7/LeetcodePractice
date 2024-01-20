4484448753857b14d48bdbfb441bdffc6504a254
shubhjena <117462852+shubhjena@users.noreply.github.com>
2024-01-21 00:08:57 +0530

    add new approach for FindTheDuplicateNumber in Arrays package

--- src/Arrays/FindTheDuplicateNumber.java
+++ src/Arrays/FindTheDuplicateNumber.java
@@ -22,8 +22,18 @@ public class FindTheDuplicateNumber {
         return slow;
     }
 
+    public static int findDuplicate2(int[] nums) {
+        for(int i=0; i<nums.length ; i++){
+            int ind = Math.abs(nums[i]);
+            if(nums[ind]<0) return ind;
+            nums[ind]*=-1;
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1,3,4,2,2};
-        System.out.println("Duplicate number is: "+findDuplicate(nums));
+        System.out.println("//1 Duplicate number is: "+findDuplicate(nums));
+        System.out.println("//2 Duplicate number is: "+findDuplicate2(nums));
     }
 }
